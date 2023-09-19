5250910373cdf337a9f6c33a02ae085bb430a279
SrajalDixit <srajaldixit01@gmail.com>
2023-09-20 01:48:13 +0530

    nothing much

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,20 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
+import 'package:instagram_clone/widgets/follow_button.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  final String uid;
+  const ProfileScreen({Key? key, required this.uid}) : super(key:key);
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+var userData ={};
+  @override
+  void initState() {
+    
+    super.initState();
+    getData();
+  }
+
+  getData() async{
+    try{
+      var snap = await FirebaseFirestore.instance.collection('users').doc(widget.uid).get();
+      userData = snap.data()!;
+      setState(() {
+        
+      });
+    }
+    catch(e)
+    {
+      showSnackBar(e.toString(),context);
+
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
-        title: const Text('username'),
+       title: Text(userData['username'] ?? 'Username'),
+
       ),
       body: ListView(
         children: [
@@ -26,51 +53,87 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   children: [
                     CircleAvatar(
                       backgroundImage: NetworkImage(
-                          'https://images.unsplash.com/photo-1694746028047-b105a3543137?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
+                          userData['photoUrl'] ?? 'profile pic',),
                       radius: 40,
                     ),
-                     Expanded(
-                       child: Row(
-                                       mainAxisSize: MainAxisSize.min,
-                                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                     
-                                       children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
                               buildStateColumn(50, 'posts'),
                               buildStateColumn(510, 'followers'),
                               buildStateColumn(300, 'following'),
-                     
-                                       ],
-                                     ),
-                     )
-
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              FollowButton(
+                                backgroundColor: mobileBackgroundColor,
+                                borderColor: Colors.grey,
+                                text: 'Edit profile',
+                                textColor: primaryColor,
+                                function: () {},
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-               
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Text(
+                    'username',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(top: 1),
+                  child: Text(
+                    'Some Description',
+                    
+                  ),
+                ),
               ],
             ),
-          )
+            
+          ),
+          const Divider(),
         ],
       ),
     );
   }
-  Column buildStateColumn(int num,String label)
-  {
-    
+
+  Column buildStateColumn(int num, String label) {
     return Column(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(num.toString(),
-        style: const TextStyle(
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
-        ),),
-        Text(label.toString(),
-        style: const TextStyle(
-          color: Colors.grey,
-          fontWeight: FontWeight.w400,
-        ),)
-        
+        Text(
+          num.toString(),
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 4),
+          child: Text(
+            label.toString(),
+            style: const TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        )
       ],
     );
   }
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,11 +1,17 @@
-
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screen.dart';
 import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/screens/search_screen.dart';
 
-const WebScreenSize=600;
+const WebScreenSize = 600;
 
-const homeScreenItems =  [feedScreen(),SearchScreen(),AddPostScreen(),Text('notification'),ProfileScreen()];
+// Create instances of the widget classes here
+final List<Widget> homeScreenItems = [
+  feedScreen(),
+  SearchScreen(),
+  AddPostScreen(),
+  Text('notification'),
+  ProfileScreen(uid: FirebaseAuth.instance.currentUser!.uid),
+];
